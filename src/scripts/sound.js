8f6e05aa741140eb9ff15741cf481f58a7c11037
Edmond Hui <edmond.t.hui@gmail.com>
2020-12-09 20:55:51 -0500

    :sparkles: update eating sound

--- src/scripts/sound.js
+++ src/scripts/sound.js
@@ -8,12 +8,13 @@ class Sound {
 
   play() {
     let sound = this.sound.cloneNode(true)
-    sound.volume = .2
+    // sound.volume = .2
     sound.play();
+    // this.sound.play();
   }
 
   repeatPlay() {
-    this.sound.volume = .1
+    this.sound.volume = .05
     this.sound.id = "music"
     this.sound.addEventListener('ended', () => {
       this.currentTime = 0;
